e082d8d5178fe0ba78cd0105ad06bfaf63974dc9
Anna <annafaix@gmail.com>
2019-05-16 17:22:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-box">
+    <p>Logga in med personlig kod</p>
+    <input type="password" placeholder="Ange din kod"/>
+    <div className="digits">
+
+    </div>
+    <div className="buttons-submit-cancel">
+      <button id="submit-button">Logga in</button>
+      <button id="cancel-button">X</button>
+    </div>
+   
     </div>
   );
 }
