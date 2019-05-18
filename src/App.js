f4f1ef5e5a56a19c985c701a868f39d9e96b7f43
annafaix <annafaix@gmail.com>
2019-05-18 12:20:43 +0200

    Added digits, functions to console log/delete value

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+function Digits(){
+  let numbersArray= [0,1,2,3,4,5,6,7,8,9];
+  return (
+    <div className="digits">
+      {numbersArray.map((value, index) => {
+        return <button onClick={this.props.hanleEvent} key={index}>{value}</button>
+      })}
+    </div>
+  )
+}
+
 function App() {
+  let numbersArray= [0,1,2,3,4,5,6,7,8,9,null, null, null, null,null, null];
+  let arrayLength = 16;
+  let [inputValue, setInputValue] = useState("")
+
+  function handleInputChange(e){
+    setInputValue(e.target.value)
+  }
+  function handleDigitsButtons(e){
+    setInputValue(inputValue + e.target.value)
+  }
+  function submitLogIn(){
+    console.log(`Din personliga kod: ${inputValue}`)
+  }
+  function deleteValue(){
+    setInputValue(inputValue.slice(0,-1))
+  }
   return (
     <div className="main-box">
     <p>Logga in med personlig kod</p>
-    <input type="password" placeholder="Ange din kod"/>
-    <div className="digits">
-
+    <input  placeholder="Ange din kod" value={inputValue} onChange={handleInputChange}/>
+    <div className="digits-container">
+      {numbersArray.map((value, index) => {
+        return <button className="digit-button" onClick={handleDigitsButtons} value={value} key={index}>{value}</button>
+      })}
     </div>
     <div className="buttons-submit-cancel">
-      <button id="submit-button">Logga in</button>
-      <button id="cancel-button">X</button>
+      <button id="submit-button" onClick={submitLogIn}>Logga in</button>
+      <button id="cancel-button" onClick={deleteValue}>X</button>
     </div>
    
     </div>
